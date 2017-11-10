cdec2d6c030e0c99b165346946aed62cc4e70268
RJ Diepenbrock <rjdwebsites@gmail.com>
2017-11-10 11:53:42 -0600

    WIP. Website build.
    Signed-off-by: RJ Diepenbrock <rjdwebsites@gmail.com>

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Real Play Gaming</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+
+<div class="parallax"></div>
+
+<div class="logo">
+	<div class="btn btn-dark">
+		<h1 class="text-white">Real Play Gaming</h1>
+	</div>
+</div>
+
+<div class="bg-white">
+	<div class="container">
+		<div class="row py-4">
+
+			<h2>About Us</h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras convallis nisl velit, ac pretium ipsum hendrerit et. Aliquam in massa turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel vulputate nibh, eu efficitur ligula. Morbi ultricies sagittis ligula, eget cursus arcu pretium at. Aenean laoreet, velit quis porttitor malesuada, libero ex vulputate felis, ac laoreet lectus lacus a ante. Mauris ultrices elit tellus, ac luctus nibh lacinia eget. Integer eu mattis ante, lobortis ultricies libero. Sed placerat ligula vitae rhoncus maximus.</p>
+
+			<div class="clearifx col-8">
+
+				<h3>The Team</h3>
+				<ul class="list-inline">
+					<li class="list-inline-item">
+						Jon Roberts
+					</li>
+					<li class="list-inline-item">Jason Roelofs</li>
+					<li class="list-inline-item">R.J. Diepenbrock</li>
+				</ul>
+
+			</div>
+
+			<img src="assets/images/gotcha_game.png" class="img-responsive gotcha-game" alt="Play Gotcha!">
+
+		</div>
+	</div>
+</div>
+
+<div class="bg-secondary">
+	<div class="container">
+		<div class="row py-4">
+
+			<h2>Contact Us</h2>
+
+			<form method="POST" action="" name="contact" class="col-12 py-4">
+				<div class="form-group">
+					<input type="text"
+						   name="fullname"
+						   class="form-control"
+						   placeholder="Enter your full name"
+						   aria-label="Enter your full name"
+					>
+				</div>
+				<div class="form-group">
+					<input type="email"
+						   name="email"
+						   class="form-control"
+						   placeholder="Enter your email address"
+						   aria-label="Enter your email address"
+					>
+				</div>
+
+				<button type="submit" class="btn btn-primary" name="submit">Send</button>
+			</form>
+
+		</div>
+	</div>
+</div>
+
+<footer class="bg-dark">
+	<div class="container">
+		<div class="row py-3 text-white">
+			<div class="col-4">&copy; Copyright 2017</div>
+
+			<div class="col-8 text-right">
+				<a href="#">Privacy Policy</a>
+				&nbsp;|&nbsp;
+				<a href="#">Terms &amp; Conditions</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
